7bf8252fb0ed6a65f9bb77edb64e856307634caa
panlatent <panlatent@gmail.com>
2014-10-16 09:07:28 +0800

    Add Str::convertCamel and Str::convertSnake

--- src/Str.php
+++ src/Str.php
@@ -1,7 +1,42 @@
 <?php
 
-namespace paboost;
+namespace paboost {
 
-class Str {
+	class Str {
 
+		/**
+		 * Convert a string to camle case.
+		 *
+		 * @param string $str
+		 * @param array  $separators
+		 * @param string $delimiter
+		 * @return string
+		 */
+		public static function convertCamel($str, $separators = ['_', '-'], $delimiter = '')
+		{
+			$str = ucwords(str_replace($separators, ' ', $str));
+
+			return str_replace(' ', $delimiter, $str);
+		}
+
+		/**
+		 * Convert a string to snake case.
+		 *
+		 * @param string $str
+		 * @param string $delimiter
+		 * @param bool   $each
+		 * @return string
+		 */
+		public static function convertSnake($str, $delimiter = '_', $each = true)
+		{
+			if (ctype_lower($str)) return $str;
+
+			$replace = $each ? '$1' . $delimiter : '$1' . $delimiter . '$2';
+			$pattern = $each ? '/(.)(?=[A-Z])/' : '/(.)([A-Z]+)/';
+
+			return strtolower(preg_replace($pattern, $replace, $str));
+		}
+
+	}
+	
 }
---
+++ tests/StrTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Test {
+
+	use paboost\Str;
+
+	class StrTest extends \PHPUnit_Framework_Testcase {
+
+		public function testConvertCamel()
+		{
+			$this->assertEquals('PaboostOnGithub', Str::convertCamel('paboost_on_github'));
+        	$this->assertEquals('PaboostOnGithub', Str::convertCamel('paboost-on-github'));
+        	$this->assertEquals('Paboost-On-Github', Str::convertCamel('paboost-on-github', ['-'], '-'));
+		}
+
+		public function testConvertSnake()
+		{
+			$this->assertEquals('boost_p_h_p_libray', Str::convertSnake('BoostPHPLibray'));
+    		$this->assertEquals('boost-p-h-p-libray', Str::convertSnake('BoostPHPLibray', '-'));
+    		$this->assertEquals('boost-phplibray', Str::convertSnake('BoostPHPLibray', '-', false));
+		}
+
+	}
+
+}
